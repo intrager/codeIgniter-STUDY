7e0c18d3a1128baeb69bfedcebcff4b29cff4e22
intrager <intrager@naver.com>
2021-12-01 00:32:05 +0900

    복구 실수함..

--- sale27/html/application/controllers/Main1.php
+++
@@ -1,9 +0,0 @@
-<?
-	class Main1 extends CI_Controller {               // 클래스이름 첫 글자는 대문자
-		public function index()                      // 제일 먼저 실행되는 함수
-		{
-			$this->load->view("main_header1");		// view폴더의 main_header1.php 와
-			$this->load->view("main_footer1");		//  main_footer1.php 호출
-		}
-	}
-?>
