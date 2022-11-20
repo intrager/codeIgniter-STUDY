35e4f7133111e85806f3cb2f85217e925615d3f6
Bruce Han <intrager@naver.com>
2022-11-20 16:20:53 +0900

    Delete sale27/html directory

--- sale27/html/application/views/picture_zoom.php
+++
@@ -1,9 +0,0 @@
-
-<div class="container">
-	<div class="mythumb_box">
-		<a href="javascript:window.close();">
-			<img src="/~sale27/product_img/<?=$iname; ?>" class="mythumb_bigimage" >
-		</a>
-		<div class="mythumb_text"><?=$pname; ?></div>
-	</div>
-</div>
